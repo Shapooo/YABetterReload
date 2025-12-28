87c86178a3c0f3719982c2e0d230d49efc0693ba
Shapooo <ShaboJia@outlook.com>
2025-12-28 17:47:43 +0800

    patch ItemSetting_Gun.AutoSetTypeInInventory

---
+++ Patches/AutoSetTypePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using ItemStatsSystem;
+using System;
+
+namespace YABetterReload.Patches
+{
+    [HarmonyPatch(typeof(ItemSetting_Gun), "AutoSetTypeInInventory")]
+    internal static class AutoSetTypePatch
+    {
+        private static void Postfix(ItemSetting_Gun __instance, Inventory inventory, ref bool __result)
+        {
+            Item? ammoItem;
+            if (__instance == null || __result || !ReloaderCore.IsPlayerInventory(inventory) || !ReloaderCore.LocateCompatibleAmmo(__instance, out ammoItem))
+                return;
+            __instance.SetTargetBulletType(ammoItem);
+            __result = true;
+            return;
+        }
+    }
+}
