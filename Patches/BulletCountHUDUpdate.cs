f559c431f3c435ef0eb51396a5892af7d347b5ce
Shapooo <ShaboJia@outlook.com>
2025-12-29 11:21:14 +0800

    patch BulletCountHUD.Update

---
+++ Patches/BulletCountHUDUpdate.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace YABetterReload.Patches
+{
+    [HarmonyPatch(typeof(BulletCountHUD), "Update")]
+    internal class BulletCountHUDUpdate
+    {
+        private static float _lastUpdateTime = 0f;
+        private static float _updateInterval = 0.5f;
+
+        private static void Postfix(BulletCountHUD __instance)
+        {
+            if (Time.time - _lastUpdateTime > _updateInterval)
+            {
+                _lastUpdateTime = Time.time;
+                Traverse.Create(__instance).Method("ChangeTotalCount").GetValue();
+            }
+        }
+    }
+}
